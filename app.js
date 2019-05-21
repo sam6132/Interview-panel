145cff89fdd39ba3d695535384b329a8c40bfd52
sam joshuva <sam613263@gmail.com>
2019-05-21 14:55:31 +0530

    init

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const path = require('path');
-// const user = require('./routes/user');
+const user = require('./routes/user');
+// const project = require('./routes/project');
+
 
 var app = express();
 app.use(express.json())
@@ -10,13 +12,15 @@ app.use(cors())
 // app.use(express.static(path.join(__dirname, './public')))
 
 
-mongoose.connect("mongodb+srv://admin:12345@cluster0-jqe8z.mongodb.net/interview-panel?retryWrites=true", {
+mongoose.connect("mongodb://localhost:27017/ConvoBot", {
     useNewUrlParser: true
 }, () => {
     console.log("Connected to Mongodb at port 27017")
 })
 
-// app.use("/api/user", user)
+app.use("/api/user", user)
+// app.use("/api/project", project)
+
 
 
 
