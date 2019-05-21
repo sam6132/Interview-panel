bcf1ccc28867885282369a52da6fc5294effc5f7
sam joshuva <sam613263@gmail.com>
2019-05-21 15:01:04 +0530

    user(admin) module

---
+++ routes/user.js
@@ -0,0 +1,169 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
+const auth = require('../middleware/auth')
+
+mongoose.set('useCreateIndex', true);
+const users = mongoose.model(
+    'User',
+    new mongoose.Schema({
+        email: {
+            type: String,
+            required: true,
+            minlength: 5,
+            unique: true,
+            maxlength: 30
+        },
+        password: {
+            type: String,
+            required: true,
+
+        },
+        role: {
+            type: String,
+            required: true,
+
+        }
+    })
+);
+
+//Get all user
+router.get('/', async (req, res) => {
+    const user = await users.find().sort('name');
+    res.send(user);
+});
+
+//get user by id
+router.get('/:id', async (req, res) => {
+    const user = await users.findOne({
+        _id: req.params.id
+    });
+    if (!user) return res.status(400).send('no such user register');
+    res.send(user);
+});
+
+//Login user by name
+router.post('/login', async (req, res) => {
+    const user = await users.findOne({
+        email: req.body.email
+    });
+    if (!user)
+        return res.json({
+            success: false,
+            msg: 'User not found'
+        });
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) throw err;
+
+        if (!result)
+            return res.json({
+                success: false,
+                msg: 'Wrong password'
+            });
+
+        const token = jwt.sign(JSON.stringify(user), 'jwtPrivateKey');
+        res.header('x-auth', token).json({
+            success: true,
+            token: token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    });
+});
+
+//Register a user
+router.post('/register', async (req, res) => {
+    const {
+        error
+    } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await users.findOne({
+        email: req.body.email
+    });
+
+    if (user) {
+        return res.json({
+            success: false,
+            message: 'User Already Rgistered'
+        });
+    }
+
+    user = new users({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    user
+        .save()
+        .then(result => {
+            res.json({
+                success: true,
+                user: _.pick(result, ['name', 'email'])
+            });
+        })
+        .catch(err => {
+            res.json({
+                success: false,
+                user: 'Cannot create a Account'
+            });
+        });
+});
+
+//update user
+router.put('/:id', async (req, res) => {
+    const user = await users.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+
+    res.json({
+        success: true,
+        message: 'Updated Sucessfully'
+    });
+});
+
+// delete user
+router.delete('/:id', async (req, res) => {
+    user = await users.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(400).send('no such user register');
+
+    res.json({
+        success: true,
+        message: 'Deleted Sucessfully'
+    });
+});
+
+// validate user object from front end
+function validateUser(user) {
+    const schema = {
+        name: Joi.string()
+            .min(5)
+            .max(50)
+            .required(),
+        email: Joi.string()
+            .min(5)
+            .max(50)
+            .required(),
+        password: Joi.string()
+            .min(5)
+            .max(50)
+            .required()
+    };
+
+    return Joi.validate(user, schema);
+}
+
+module.exports = router;
