dd968a31315e697b755e19d1d9dcfad80cc0b86a
mdvenkatesh <mdvenkatesh1421@gmail.com>
2019-06-01 16:17:46 +0530

    changes in roundde

--- client/src/views/pages/RoundDetails.jsx
+++ client/src/views/pages/RoundDetails.jsx
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import MaterialTable from 'material-table';
 import { getCandidates } from 'services/candidate';
 import { getAccessToken } from 'services/auth';
 import { editCandidateById } from 'services/candidate';
 import {editRounddetails} from 'services/candidate';
+import { deleteCandidateById } from 'services/candidate';
 import { getRoundDetailsByCandidateId } from 'services/candidate';
 import { updateRoundDetailsByCandidateId } from 'services/candidate';
 import {addrounds} from 'services/candidate';
-
+import {addrounds} from 'services/candidate';
 
 const columns = [
 	{ title: 'round', field: 'title' },
 	{ title: 'comment', field: 'comment' },
-	{ title: 'qualified', field: 'qualified', lookup: { true: 'yes', false: 'no' } }
+	{ title: 'qualified', field: 'qualified', lookup: { true: 'yes', false: 'no' } },
+	{ title: 'qualified', field: 'qualified', }
 ];
 
 export default class RoundDetailTable extends Component {
 	state = {
 		rounddetails: [],
 		candidate_id: ''
+		candidate_id:''
 	};
 
 	componentDidMount() {
+
 		this.getReview();
 	}
 
 	getReview = async () => {
 		console.log('we are printhing props');
+		console.log("we are printhing props");
 		console.log(this.props.candidate_id);
 
 		// await this.setState({candidate_id : this.props.candidate_id})
@@ -35,46 +38,41 @@ export default class RoundDetailTable extends Component {
 			.then(response => {
 				console.log(response.data);
 				this.setState({ rounddetails: response.data });
+				console.log(response.data)
+				this.setState({rounddetails:response.data})
 			})
 			.catch(error => {
 				console.log(error);
 			});
 	};
 	// have to write put api
+  // have to write put api 
 	// navEdit = (e, data) => {
 	// 	console.log(data);
 	// 	this.props.history.push(`/updateRoundDetailsByCandidateId/${data._id}`);
-	// };
-
-	render() {
-		return (
-			<MaterialTable
+@ -52,9 +54,7 @@ export default class RoundDetailTable extends Component {
 				title="Rounds List"
 				columns={columns}
 				data={this.state.rounddetails}
 				onRowClick={e => {
 					console.log(e);
 				}}
+				onRowClick={this.navEdit}
 				editable={{
 					onRowAdd: newData =>
 						new Promise(resolve => {
-							setTimeout(() => {
-								resolve();
-								// addCandidate(newData)
-								  addrounds(this.props.candidate_id,newData).then(res => {
-										let data = res.data;
-										console.log(data);
+@ -67,8 +67,8 @@ export default class RoundDetailTable extends Component {
 										if (data.success === false) return this.setState({ msg: data.message });
 										this.getReview();
 									})
 									.catch(err => {
 										console.log(err.message);
+									.catch(error => {
+										this.setState({ msg: error.message });
 									});
 							}, 600);
 						}),
-                     // THESE is object type which we call by key value type
-					onRowUpdate: (newData, oldData) =>
-					 // creating a promise
+@ -78,17 +78,27 @@ export default class RoundDetailTable extends Component {
 						new Promise(resolve => {
 							setTimeout(() => {
 								resolve();
@@ -89,11 +87,31 @@ export default class RoundDetailTable extends Component {
 										console.log(err.message);
 									});
 							}, 600);
+								 console.log('new Candidate :', newData);
+								console.log('old Candidate :', oldData);
+								 console.log(newData._id);	
+								 console.log() 
+								 updateRoundDetailsByCandidateId(newData._id, newData)
+								 	.then(res => {
+										 console.log(res);
+								 		console.log(this.props.candidate_id);
+								 		this.getReview();
+								 	})
+								 	.catch(err => {
+								 		console.log(err.msg);
+								 	});
+								//  console.log(newData)
+							 	// this.getReview().then(res=>{
+							 	 	// console.log(res)
+							 	 	// this.getReview()
+							 	// }).catch(err=>{
+								 	// console.log(err.message)
+							 	//  })
+							}, 6000);
 						}),
 					onRowDataChange: data => {
 						console.log(data);
-					},
-					onRowDelete: oldData =>
+@ -97,14 +107,10 @@ export default class RoundDetailTable extends Component {
 						new Promise(resolve => {
 							setTimeout(() => {
 								resolve();
@@ -105,10 +123,10 @@ export default class RoundDetailTable extends Component {
 									.catch(err => {
 										console.log(err.message);
 									});
+								// data 
+								const data = [...this.state.candidates];
+								 data.splice(data.indexOf(oldData), 1);
+								 this.setState({ ...this.state, data });
 							}, 600);
 						})
 				}}
-			/>
-		);
-	}
-}
